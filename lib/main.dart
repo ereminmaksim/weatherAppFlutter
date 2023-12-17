328dfcb044c4317828f1755fd030a8d35fd3d10c
Maksim Eremin <Eryominma@sibintek.ru>
2023-12-17 13:16:04 +0300

    cubit_flutter - на данном этапе подготовлен слайдер/вертикальное пролистывание

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(
+              color:Colors.amber
+            ),
+            elevation: 0,
+            backgroundColor: Colors.deepPurple
+          )
+        ),
+        home: const WelcomePage());
+  }
+}
