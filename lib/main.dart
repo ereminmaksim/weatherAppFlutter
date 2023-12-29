77b476b82c0ee26d4e47e0e9a4fea24861dee846
Maksim Eremin <Eryominma@sibintek.ru>
2023-12-29 14:04:49 +0300

    cubit_flutter - создали начальную подготовку для архитектурного паттерна Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/cubit/app_cubits.dart';
 import 'package:flutter_cubit/pages/detail_page.dart';
 import 'package:flutter_cubit/pages/navpages/main_page.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
+
+import 'cubit/app_cubit_logic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,14 +20,13 @@ class MyApp extends StatelessWidget {
         title: 'Flutter',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            iconTheme: IconThemeData(
-              color:Colors.amber
-            ),
-            elevation: 0,
-            backgroundColor: Colors.deepPurple
-          )
-        ),
-        home: const DetailPage());
+            appBarTheme: const AppBarTheme(
+                iconTheme: IconThemeData(color: Colors.amber),
+                elevation: 0,
+                backgroundColor: Colors.deepPurple)),
+        home: BlocProvider<AppCubits>(
+          create: (context) => AppCubits(),
+          child: const AppCubitLogics(),
+        ));
   }
 }
